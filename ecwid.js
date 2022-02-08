a7b039c9c6c306d022c60eab5e93b38ea6f51d19
OliverSRieke <54849302+OliverSRieke@users.noreply.github.com>
2022-02-08 15:35:13 +0100

    Update ecwid.js

--- ecwid.js
+++ ecwid.js
@@ -1,1 +1,16 @@
-console.log('extra_field_test_chgd');
+// Initialize extra fields
+ec = ec || {};
+ec.order = ec.order || {};
+ec.order.extraFields = ec.order.extraFields || {};
+
+// Add a new optional text input 'How should we sign the package?' to shipping address form
+ec.order.extraFields.wrapping_box_signature = {
+    'title': 'Weitergabe der E-Mail-Adresse an Versanddienstleister',
+    'textPlaceholder': 'Nicht Gestattet',
+    'type': 'text',
+    'tip': 'Nur wenn Sie die Weitergabe gestatten können wir Sie über den Versandstatus informieren.',
+    'required': false,
+    'checkoutDisplaySection': 'shipping_address'
+};
+
+Ecwid.refreshConfig && Ecwid.refreshConfig();
