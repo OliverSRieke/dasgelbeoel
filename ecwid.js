8af0064714b8f69dd197eea69a0529659c775fa1
OliverSRieke <54849302+OliverSRieke@users.noreply.github.com>
2022-04-18 20:06:28 +0200

    Update ecwid.js

--- ecwid.js
+++ ecwid.js
@@ -7,7 +7,7 @@ ec.order.extraFields = ec.order.extraFields || {};
 ec.order.extraFields.wrapping_box_signature = {
     'title': 'Weitergabe der E-Mail-Adresse an Versanddienstleister',
     'type': 'checkbox',
-    'options': [{'title':'Ich stimme der Weitergabe zu'}],
+    'options': [{'title':'Ich willige ein, dass meine E-Mail-Adresse im Rahmen der Vertragsabwicklung an den Versanddienstleister weitergegeben wird, um über den Status der Lieferung informiert zu werden. Dieser Verwendung der E-Mail-Adresse kann jederzeit durch eine Mitteilung an uns widersprochen werden. Die Kontaktdaten für die Ausübung des Widerspruchs finden Sie im Impressum.'}],
     'required': false,
     'checkoutDisplaySection': 'shipping_address'
 };
