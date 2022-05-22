e130a0db031aa31fbe043d036b94e5873eb4a576
OliverSRieke <54849302+OliverSRieke@users.noreply.github.com>
2022-05-22 15:54:55 +0200

    Update shopauskunft.js

--- shopauskunft.js
+++ shopauskunft.js
@@ -1,1 +1,13 @@
+
+<style scoped>
+@media only screen and (min-width: 768px)  {
+    div#shopauskunft_widget{
+        display: block;
+        position: fixed;
+        right: 10px;
+        bottom: 10px;
+    }
+}
+</style>
+
 <!-- Shopauskunft Widget --><div id="shopauskunft_widget"><a href="https://www.shopauskunft.de/review/shop.dasgelbeoel.de" target="_blank"><img src="https://apps.shopauskunft.de/widget/v5/seal.php?token=4ff5f5c653c018505b116d8109c998b5&w=160&cut_headline=0&cut_text=0&cut_count=0&cut_date=0" border="0" alt="ShopAuskunft.de Siegel" width="160" height="200" /></a></div><!-- Shopauskunft Widget End -->
